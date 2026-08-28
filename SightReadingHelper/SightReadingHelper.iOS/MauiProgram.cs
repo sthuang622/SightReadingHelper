4c1d56db40397ade66384e9dca760be421a417b2
sth622 <sthuang622@gmail.com>
2026-08-28 21:41:01 +0800

    Add project files.

---
+++ SightReadingHelper/SightReadingHelper.iOS/MauiProgram.cs
@@ -0,0 +1,15 @@
+﻿namespace SightReadingHelper.iOS
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+
+            builder
+                .UseSharedMauiApp();
+
+            return builder.Build();
+        }
+    }
+}
---
+++ SightReadingHelper/SightReadingHelper/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace SightReadingHelper
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
